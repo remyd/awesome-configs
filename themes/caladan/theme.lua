1de974c6d8fdf1b5e43c4a08f12b21e97e89fd70
Remy Dautriche <remy.dautriche@gmail.com>
2018-10-09 20:38:43 +0200

    Modified layout icons for cleaner look

--- themes/caladan/theme.lua
+++ themes/caladan/theme.lua
@@ -55,19 +55,9 @@ theme.calendar = confdir .. "/icons/calendar.png"
 theme.wallpaper_cmd = { "awsetbg /home/remy/images/wallpapers/rescape_starstrooper.jpg" }
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
-theme.layout_fairv = "/usr/share/awesome/themes/default/layouts/fairvw.png"
-theme.layout_floating  = "/usr/share/awesome/themes/default/layouts/floatingw.png"
-theme.layout_magnifier = "/usr/share/awesome/themes/default/layouts/magnifierw.png"
-theme.layout_max = "/usr/share/awesome/themes/default/layouts/maxw.png"
-theme.layout_fullscreen = "/usr/share/awesome/themes/default/layouts/fullscreenw.png"
-theme.layout_tilebottom = "/usr/share/awesome/themes/default/layouts/tilebottomw.png"
-theme.layout_tileleft   = "/usr/share/awesome/themes/default/layouts/tileleftw.png"
-theme.layout_tile = "/usr/share/awesome/themes/default/layouts/tilew.png"
-theme.layout_tiletop = "/usr/share/awesome/themes/default/layouts/tiletopw.png"
-theme.layout_spiral  = "/usr/share/awesome/themes/default/layouts/spiralw.png"
-theme.layout_dwindle = "/usr/share/awesome/themes/default/layouts/dwindlew.png"
-
-theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
+theme.layout_floating = confdir .. "/icons/layouts/floating.png"
+theme.layout_max = confdir .. "/icons/layouts/max.png"
+theme.layout_tile = confdir .. "/icons/layouts/tile.png"
+theme.layout_tiletop = confdir .. "/icons/layouts/tiletop.png"
 
 return theme
